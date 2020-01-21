3df221ec797e57b11c7f708d8ca156a7cf86acfa
chongfenghaotest <572423417@qq.com>
2020-01-21 13:57:35 +0800

    first

---
+++ src/main/java/cn/arua/ControllerApp.java
@@ -0,0 +1,19 @@
+package cn.arua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ControllerApp {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello ... world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApp.class,args);
+    }
+}
---
+++ src/main/java/cn/arua/service/Person.java
@@ -0,0 +1,5 @@
+package cn.arua.service;
+
+public class Person {
+
+}
