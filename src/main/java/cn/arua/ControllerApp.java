22953bddc7096d7669879e366b91538652e44839
chongfenghaotest <572423417@qq.com>
2020-01-22 13:00:18 +0800

    name v.6

--- src/main/java/cn/arua/ControllerApp.java
+++ src/main/java/cn/arua/ControllerApp.java
@@ -2,12 +2,14 @@ package cn.arua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ControllerApp {
+public class ControllerApp extends SpringBootServletInitializer {
     @RequestMapping("/hello")
     public String hello(){
         return "hello ... world -v.0.1";
@@ -16,4 +18,10 @@ public class ControllerApp {
     public static void main(String[] args) {
         SpringApplication.run(ControllerApp.class,args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ControllerApp.class);
+    }
+
 }
